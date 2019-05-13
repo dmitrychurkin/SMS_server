8c67238ef80b93baff3ca10736b5ac8946d904c7
Dmitry Churkin <dmitrychurkin1988@gmail.com>
2019-05-13 14:43:54 +0300

    amended implementation base on Dima's advice

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 module.exports = {
     smsController: require('./sms'),
-    IOClearDevice: require('./clear-device.io'),
-    IORecordDevice: require('./record-device.io')
+    IOConnect: require('./connect.io'),
+    IODisconnect: require('./disconnect.io')
 };
